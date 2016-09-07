4208fa12e7e0a02636c3b19530fdec4c22f335e7
Thomas Frost <thomasfrost@protonmail.com>
2016-09-07 14:46:51 +0200

    Singleton prototype

---
+++ ProofOfConcept/DesignPatterns/Creational/Singleton/SingleObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOfConcept.DesignPatterns.Creational.Singleton
+{
+    public class SingleObject
+    {
+        private static SingleObject instance;
+
+        static SingleObject()
+        {
+            instance = new SingleObject();
+        }
+
+        private SingleObject() { }
+
+        public static SingleObject GetInstance()
+        {
+            return instance;
+        }
+
+        public override string ToString()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ ProofOfConcept/DesignPatterns/Creational/SingletonDemo.cs
@@ -0,0 +1,15 @@
+﻿namespace ProofOfConcept.DesignPatterns.Creational
+{
+    public class SingletonDemo
+    {
+
+        public static void TestSingleton()
+        {
+            // Error - private constructor!
+            //var s = new Singleton.SingleObject();
+
+            var s = Singleton.SingleObject.GetInstance();
+            System.Console.WriteLine(s.ToString());
+        }
+    }
+}
