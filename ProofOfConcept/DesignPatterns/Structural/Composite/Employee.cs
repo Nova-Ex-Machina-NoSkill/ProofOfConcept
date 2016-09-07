4e61eba071b57bab8a57785023b72da7e8564eb1
Thomas Frost <thomasfrost@protonmail.com>
2016-09-07 19:15:43 +0200

    Composite pattern

---
+++ ProofOfConcept/DesignPatterns/Structural/Composite/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOfConcept.DesignPatterns.Structural.Composite
+{
+    public class Employee
+    {
+        private string name;
+        private string dept;
+        private int salary;
+        private List<Employee> subordinates;
+
+        public List<Employee> Subordinates { get { return subordinates; } }
+
+        public Employee(string name, string dept, int salary)
+        {
+            this.name = name;
+            this.dept = dept;
+            this.salary = salary;
+            subordinates = new List<Employee>();
+        }
+
+        public void Add(Employee e)
+        {
+            subordinates.Add(e);
+        }
+
+        public void Remove(Employee e)
+        {
+            subordinates.Remove(e);
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: [{name}, Dep: {dept}, Salary: {salary}]";
+        }
+    }
+}
---
+++ ProofOfConcept/DesignPatterns/Structural/CompositeDemo.cs
@@ -0,0 +1,27 @@
+﻿namespace ProofOfConcept.DesignPatterns.Structural
+{
+    using Composite;
+
+    public class CompositeDemo
+    {
+        public static void TestComposite()
+        {
+            var ceo = new Employee("Thomas Frost", "CEO", 1337);
+            var sales = new Employee("Chase", "Sales", 3000);
+            var s1 = new Employee("Robert", "Sales", 2500);
+            var s2 = new Employee("Michael", "Sales", 2750);
+            var marketing = new Employee("Lisa", "Marketing", 4000);
+            var m1 = new Employee("Janet", "Marketing", 1000);
+            var m2 = new Employee("Jacson", "Marketing", 1500);
+            ceo.Add(sales);
+            ceo.Add(marketing);
+            sales.Add(s1);
+            sales.Add(s2);
+            marketing.Add(m1);
+            marketing.Add(m2);
+            System.Console.WriteLine(ceo.ToString());
+            foreach (Employee e in sales.Subordinates) System.Console.WriteLine(e.ToString());
+            foreach (Employee e in marketing.Subordinates) System.Console.WriteLine(e.ToString());
+        }
+    }
+}
